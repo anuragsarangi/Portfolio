d696d9c94c98e1e63328c219359b5f6b170b35df
Anurag Sarangi <70016059+anuragsarangi@users.noreply.github.com>
2024-12-26 19:12:43 +0530

    Update form-submission-handler-New.js

--- form-submission-handler-New.js
+++ form-submission-handler-New.js
@@ -52,7 +52,7 @@
 
     // add form-specific values into the data
     formData.formDataNameOrder = JSON.stringify(fields);
-    formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
+    formData.formGoogleSheetName = form.dataset.sheet || "Anurag-Sarangi-Send mail Sheet"; // default sheet name
     formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
 
     console.log(formData);
